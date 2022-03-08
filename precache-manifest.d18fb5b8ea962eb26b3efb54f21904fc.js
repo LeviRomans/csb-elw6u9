09021be21e059eb1247d0f764aacc768a191b78c
Code Sandbox <hello@codesandbox.io>
2022-03-08 17:15:58 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d18fb5b8ea962eb26b3efb54f21904fc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61b3d892a9c0cc702ef238477f80d8b2",
+    "url": "https://leviromans.github.io/csb-elw6u9/index.html"
+  },
+  {
+    "revision": "d9f728c225590374faf5",
+    "url": "https://leviromans.github.io/csb-elw6u9/static/js/2.067bdb22.chunk.js"
+  },
+  {
+    "revision": "53ee44a05b2e2c0d4ebf",
+    "url": "https://leviromans.github.io/csb-elw6u9/static/js/main.c4e7f20e.chunk.js"
+  },
+  {
+    "revision": "0e8f1d9e6a04c3b59583",
+    "url": "https://leviromans.github.io/csb-elw6u9/static/js/runtime-main.b97c8103.js"
+  }
+]);
